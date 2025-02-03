ff356afd1e43c9898fed0efc178992ca76a56e3b
Mohamed_Wael <mohamedrakha808@gmail.com>
2025-02-03 18:08:48 +0200

    version 4.9.9.7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1543242205"
+    serviceWorkerVersion: "2888466190"
   }
 });
